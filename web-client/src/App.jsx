030369c3c60d3c37fd1eefff258dc8ad86c54dcf
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-06-16 15:29:38 -0400

    update bg

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-     <Routes>n
+     <Routes>
       <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
       <Route path='/login' element={authUser ? <Navigate to="/" /> : <LoginPage />} />
       <Route path='/signup' element={authUser ? <Navigate to="/" /> : <SignupPage />} />
