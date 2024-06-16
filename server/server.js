e2d60949bb731cb7206f81b82f7f4b024f1a4a5c
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-06-16 15:49:19 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 
 //routes
 import authRoutes from "./routes/authRoutes.js"
@@ -15,6 +17,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 
 
 // MiddleWare
@@ -25,6 +29,12 @@ app.use(cookieParser()); // Parse cookies from req.cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, "/web-client/dist")))
+
+app.get("*", (req, res) => (
+  res.sendFile(path.join(__dirname, "web-client", "dist", "index.html"))
+))
 
 server.listen(PORT, () => {
   connectToMongoDB();
