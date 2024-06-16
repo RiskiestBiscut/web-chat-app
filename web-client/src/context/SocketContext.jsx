209fd3358c313e859e44c33e027b6cb314ed0f6b
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-06-16 16:11:55 -0400

    update socket url

--- web-client/src/context/SocketContext.jsx
+++ web-client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://web-chat-app-j296.onrender.com", {
         query: {
           userId: authUser._id
         }
